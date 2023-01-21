f2441c22e6cc5959557c88850760e60d3c7aca4e
ajay <ajay.garg@itechnolabs.tech>
2023-01-21 09:23:33 +0530

    migration for subsciption table

---
+++ database/migrations/2022_08_27_054727_create_subscription_plans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionPlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscription_plans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('type_id');
+            $table->decimal('amount');
+            $table->string('name')->nullable();
+            $table->string('max_descount')->nullable();
+            $table->string('validaity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription_plans');
+    }
+}
